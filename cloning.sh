1b13c05315de456297846d2b3d0b3a420f1d2b63
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-23 23:25:41 +0700

    Add files via upload

---
+++ cloning.sh
@@ -0,0 +1,69 @@
+#!/system/xbin/bash
+#mau ngapain babi
+#ngerecode ? ngereedit?
+#mikir goblok pake otak
+#lo punya otak kan udah sono blajar dulu
+#ga punya etika kau ?? nyolong terus
+#kalau mau blajar pm gue aja gausah nyolong
+#haram bego
+clear
+toilet Bigbos | lolcat
+echo "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆" | lolcat
+echo
+echo "team ; cyb3r Pop1ce +62"
+echo "Penulis Bigbos Starla" | lolcat
+echo "pemakai yah kamu tolol :v"
+echo "friends ;notfriends" | lolcat
+echo
+echo "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆"
+echo
+echo "####################################" | lolcat
+echo "kontak saya"
+echo
+echo "fb Bigbos Starla" | lolcat
+echo "plees subscribe chanel saya"
+echo "chanel Bigbos Starla" | lolcat
+echo
+echo"#####################################"
+echo
+echo "♤♡♧♧♢♡♤♡♢♧♧♢♡♤♢♧♧♧♡♤♤♢♧♧♡♤♡♧♢" | lolcat
+echo
+echo "1.yahoo clone"
+echo
+echo "0.exit/kluar" | lolcat
+echo
+echo "♧♢♡♤♡♢♧♧♧♡♡♡♢♧♧♢♡♡♢♧♧♧♢♡♡♢♧♧♡" 
+read -p "R00tBySt4rla~#" jancok
+
+if [ $jancok = 1 ]
+then
+clear
+figlet Starla | lolcat
+echo "tunggu coegg"
+echo "masih loading" | lolcat
+echo "Loading 99%"
+pkg install python2
+pip install requests
+pip install mechanize
+pkg install git
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $jancok = 0 ]
+then
+clear
+figlet makasih | lolcat
+echo "makasih yah telah pakai tools kami" | lolcat
+sleep 1
+echo "Bigbos Starla"
+sleep 1
+echo "Cyb3r Pop1ce +62" | lolcat
+sleep 1
+echo "jika ingin bergabung"
+sleep 1
+echo "silahkan chat di fb" | lolcat
+sleep 1
+exit
+fi
